b06a571ecfc5ddf6f36a4ee7554d412e5d03ac0b
Tyler Gennaro <tylergennaro10@gmail.com>
2023-04-06 23:41:41 -0400

    professor page

--- client/src/scenes/professor/index.jsx
+++ client/src/scenes/professor/index.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import pb from "../../lib/pocketbase";
-import { Text } from "@nextui-org/react";
+import { Card, Row, Text } from "@nextui-org/react";
 
 export default function ProfessorPage() {
   const id = useParams().id;
@@ -20,6 +20,7 @@ export default function ProfessorPage() {
       const courses = await pb.collection("courses").getFullList({
         filter: `professor ~ "${id}"`,
         expand: "professor",
+        sort: "term",
         $cancelKey: "courses",
       });
       setCourseData(courses);
@@ -32,6 +33,44 @@ export default function ProfessorPage() {
       <Text h3 color="$gray800">
         {professorData?.department || ""}
       </Text>
+      <Row justify="space-between" className="mt-8 px-20">
+        <div className="text-center">
+          <Text h1 weight="bold">
+            {professorData?.difficulty?.toFixed(1)}
+          </Text>
+          <Text h4 color="$gray700">
+            Difficulty
+          </Text>
+        </div>
+        <div className="text-center">
+          <Text h1 weight="bold">
+            {professorData?.rating?.toFixed(1)}
+          </Text>
+          <Text h4 color="$gray700">
+            Based on {professorData?.numRatings} rating
+            {professorData?.numRatings != 1 ? "s" : ""}
+          </Text>
+        </div>
+        <div className="text-center">
+          <Text h1 weight="bold">
+            {professorData?.takeAgain}%
+          </Text>
+          <Text h4 color="$gray700">
+            Would take again
+          </Text>
+        </div>
+      </Row>
+      <div className="mt-8 flex flex-col gap-4">
+        {courseData.map((course) => (
+          <Card className="p-4" key={course.number} isPressable>
+            <Text h3>{course.name}</Text>
+            <div className="flex gap-8">
+              <Text color="$gray800">{course.number}</Text>
+              <Text color="$gray800">{course.term}</Text>
+            </div>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
