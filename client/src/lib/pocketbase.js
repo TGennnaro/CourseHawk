54208688cdc947825f82f74fbeece11d81776d56
Tyler Gennaro <tylergennaro10@gmail.com>
2023-04-22 17:43:39 -0400

    fixed build error

--- client/src/lib/pocketbase.js
+++ client/src/lib/pocketbase.js
@@ -1,6 +1,7 @@
 import PocketBase from 'pocketbase';
 
 const pb = new PocketBase("https://coursehawk-pocketbase.fly.dev");
-await pb.admins.authWithPassword(import.meta.env.VITE_PB_EMAIL, import.meta.env.VITE_PB_PASSWORD);
+pb.admins.authWithPassword(import.meta.env.VITE_PB_EMAIL, import.meta.env.VITE_PB_PASSWORD)
+	.catch(err => console.log(err));
 
 export default pb;
