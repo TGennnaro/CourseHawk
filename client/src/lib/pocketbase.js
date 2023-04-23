93e2be5d07fd60e28ec9677190305e1191c1e161
Tyler Gennaro <tylergennaro10@gmail.com>
2023-04-22 23:40:35 -0400

    fixed pocketbase permissions

--- client/src/lib/pocketbase.js
+++ client/src/lib/pocketbase.js
@@ -1,7 +1,5 @@
 import PocketBase from 'pocketbase';
 
 const pb = new PocketBase("https://coursehawk-pocketbase.fly.dev");
-pb.admins.authWithPassword(import.meta.env.VITE_PB_EMAIL, import.meta.env.VITE_PB_PASSWORD)
-	.catch(err => console.log(err));
 
 export default pb;
